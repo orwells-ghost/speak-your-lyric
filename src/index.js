41b54fa2b8b6c22bda1b07fe4898f4acf5f034d9
Ryan Kennedy <ryan+github@ryankennedy.me>
2020-02-28 19:24:39 -0800

    Fixed view on mobile. All final changes finished.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import './css/style.scss';
 const genius_base_url = 'https://api.genius.com/search?q='
 const genius_access_token = 'Nm6awKCVDYtu3iqesrN70oCIKGFP0x9NxDcdsGZ9O_wCPTPPjw1xtS1SwLRadMJ_'
 // const btn = document.querySelector('.btn')
-const input = document.querySelector('.input')
+const input = document.querySelector('.search__input')
+const spinner = document.querySelector('.search__spinner')
+const resultsElement = document.querySelector('.results__results')
 
 // SpeechRecognition is a global variable that lives in the browser.
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+
 const recognition = new window.SpeechRecognition()
 // interminResults are results that are not yet final, meaning isFinal property is false. Will return all matches as they come in
 recognition.interimResults = true
@@ -16,10 +19,22 @@ recognition.start()
 const onSpeak = e => {
 	// SpeechRecognitionResultList is an object with deep properties. First property is 0, which contains isFinal and another 0 property. That second 0 property contains transcript and confidence. As you speak, there will be a flood of SpeechRecognitionResultLists firing off.
 	const results = Array.from(e.results)
+	// console.log(results)
+
 	const transcript = results.map(result => result[0]).map(result => result.transcript)
 	const confidence = Math.floor(results[0][0].confidence * 100)
-	p.textContent = transcript + " confidence: " + confidence
-	console.log(transcript)
+	// console.log(`Confidence is ${confidence}`)
+	input.value = transcript
+	// console.log(transcript)
+
+	spinner.classList.add('visible')
+	if (results[0].isFinal) {
+		setTimeout(() => {
+			spinner.classList.remove('visible')
+			initiateSeach(transcript)
+
+		}, 1000)
+	}
 }
 
 // You're gonna need to rewrite this to allow for both manual search and verbal search
@@ -27,6 +42,7 @@ const initiateSeach = async (phrase) => {
 	// const userInput = input.value
 	const searchResults = await searchLyric(phrase)
 	console.log(searchResults)
+	renderResults(searchResults)
 }
 
 const searchLyric = async (searchTerm) => {
@@ -51,13 +67,49 @@ const searchLyric = async (searchTerm) => {
 	}
 }
 
+const renderResults = results => {
+	resultsElement.innerHTML = ""
+	results.forEach(result => {
+		renderSong(result)
+	})
+}
 
+const renderSong = song => {
+	const markup = `
+		<div class="results__card">
+			<img src="${song.img}" alt="${song.title} Image" class="results__card--img">
+			<div class="results__card--content">
+				<h3 class="results__card--title" title="${song.title}">${limitSongTitle(song.title)}</h3>
+				<h3 class="results__card--artist" title="${song.artist}">By ${limitSongTitle(song.artist)}</h3>
+				<div class="results__card--icons">
+					<a href="https://www.youtube.com/results?search_query=${song.title} by ${song.artist}" target="_blank">
+						<img src="assets/youtube-logo.svg" alt="" class="results__card--logo">
+					</a>
+					<a href="https://open.spotify.com/search/${song.title} ${song.artist}" target="_blank">
+						<img src="assets/spotify-logo.svg" alt="" class="results__card--logo">
+					</a>
+				</div>
+			</div>
+		</div>
+	`
+	resultsElement.insertAdjacentHTML('beforeend', markup)
+}
 
+const limitSongTitle = (title, limit = 20) => {
+	const newTitle = []
+	if (title.length > limit) {
+		title.split(' ').reduce((acc, cur) => {
+			if (acc + cur.length <= limit) {
+				newTitle.push(cur)
+			}
+			return acc + cur.length
+		}, 0)
+		return `${newTitle.join(' ')}...`
+	}
+	return title
+}
 
 // btn.addEventListener('click', initiateSeach)
 recognition.addEventListener('result', onSpeak)
 // After user stops speaking, recognition will end. This will restart it on the 'end' event
 recognition.addEventListener('end', recognition.start) 
-
-let p = document.createElement('p')
-document.querySelector('body').appendChild(p)
